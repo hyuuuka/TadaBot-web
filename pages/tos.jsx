2c9406bc485eb8b46c1664ab78ee53903e351c5b
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2024-04-23 22:31:52 +0100

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,18 +4,20 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ 1.1 License: The Developer grants the User a non-exclusive, non-transferable, revocable license to use the Bot for personal or non-commercial purposes, subject to these Terms.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ 1.2 Bot Functionality: The Bot provides various features and functionalities within the Discord platform. The Developer reserves the right to modify or update the Bot's features, functionalities, and availability at any time without prior notice.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ 1.3 User Responsibilities: The User agrees to use the Bot in compliance with applicable laws and regulations. The User is solely responsible for their use of the Bot and any content generated or transmitted through the Bot.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳1.4 Prohibited Activities: The User agrees not to engage in any of the following activities while using the Bot:
+
+- Violating any applicable laws, regulations, or third-party rights.
+- Reverse-engineering, decompiling, or disassembling the Bot.
+- Attempting to interfere with or disrupt the Bot's servers or networks.
+- Distributing or sharing the Bot with unauthorized third parties.
+- Using the Bot for any illegal, harmful, or fraudulent purposes.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
-<div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
-</div>
     </div>
   );
 }
