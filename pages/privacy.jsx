f2723ebe156fcc83768d2624b340ee6fe03ea023
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2024-04-23 22:18:39 +0100

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,20 +2,18 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for TadaBot</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 23/04/2024  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ To use the TadaBot bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(MANAGE SERVER, VIEW CHANNELS, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, MANANGE MESSAGES, USE EXTERNAL EMOJI, ADD REACTIONS, USE APPLICATION COMMANDS)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Every user can use the TadaBot however they want, as long as they do not intend to harm.</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ You cannot use certain commands at the same time while using TadaBot.</p>   
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>⊳ TadaBot does not save any data based on users, rather we save data of the giveaways and server information for no longer then 30 days for safety issues.</p>
         <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
-        <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ While using the bot (specifically starting and creating giveaways) you consent to sending us information about the giveaway you created/started that includes: (Prize, Duration, Winners, Host)</p> 
     
         </div>
             </div>
