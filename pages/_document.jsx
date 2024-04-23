d09d175de789b7a5768259feb22409e78da5f5ab
Hadi <86443416+vatsious@users.noreply.github.com>
2024-04-23 19:48:27 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Looking for an Advanced Giveaway Manager Bot? Meet TadaBot! Manage and optimize your server's giveaways with ease."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@TadaBot" />
+          <meta name="twitter:creator" content="@TadaBot" />
+          <meta property="og:url" content="https://tadabot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="TadaBot" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Looking for an Advanced Giveaway Manager Bot? Meet TadaBot! Manage and optimize your server's giveaways with ease."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="TadaBot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="TadaBot"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
