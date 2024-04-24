189005a68302b143349f590971d6fcf12b3803dc
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2024-04-24 15:25:18 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -46,7 +46,7 @@ Vote TadaBot</a>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"TadaBot" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about using TadaBot to create a relaxing environment where you can freely manage your own giveaways?" }}/>
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about using TadaBot to create a relaxing environment where you can freely manage your own giveaways?" }} />
             <a href="https://discord.com/oauth2/authorize?client_id=862340553899507715&permissions=2147871840&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Invite TadaBot
             </a>
