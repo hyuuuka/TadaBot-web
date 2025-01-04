e5660b7d047fad179503be6a3ec6ac7d4a1d3fc1
Hyuuuka <86443416+hyuuuka@users.noreply.github.com>
2025-01-04 14:32:03 +0100

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -2,24 +2,20 @@ import { Menu, Transition } from "@headlessui/react";
 import Link from "next/link";
 
 export default function Commands() {
-const commands = [
-  "/help",
-  "/end",
-  "/pause",
-  "/resume",
-  "/edit",
-  "/reroll",
-  "/coins",
-  "/daily",
-  "/transfer",
-  "/leaderboard",
-  "/invite",
-  "/premium",
-  "/serverinfo",
-  "/userinfo",
-  "/botinfo",
-  "/ping"
-];
+    const commands = [
+        "/appeal",
+        "/botinfo",
+        "/caseslist",
+        "/getreport",
+        "/ping",
+        "/report",
+        "/reportalert",
+        "/security-scan",
+        "/suspect-scan",
+        "/userinfo",
+        ""
+      ];
+      
 
   return (
     <>
@@ -28,24 +24,22 @@ const commands = [
       <img width="500" src="/img/bck2.png" />
       </div>
         <p className="text-xl font-medium text-white">
-          <i className="fal fa-cogs text-amber-400 mr-2" />
-         Bot Commands
+          <i className="fal fa-shield text-blue-400 mr-2" />
+         Moderation Commands
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of TadaBot.
+        View all available moderation and security commands for Discord Justice.
         </p>
 
         <div>
           <Menu as="div" className="w-full relative inline-block text-left">
           <div>
-              <Menu.Button className="left-0 transition w-full duration-200 hover:bg-opacity-50 bg-black bg-opacity-30 text-white focus:text-amber-400 rounded-xl border border-white/10 focus:border-amber-400 focus:outline-none py-4 px-6 flex justify-between items-center">
+              <Menu.Button className="left-0 transition w-full duration-200 hover:bg-opacity-50 bg-black bg-opacity-30 text-white focus:text-blue-400 rounded-xl border border-white/10 focus:border-blue-400 focus:outline-none py-4 px-6 flex justify-between items-center">
                 <div className="flex items-center gap-x-1 text-white text-opacity-60">
-                 
                     <>
-                      <i className="fa fa-hashtag text-white text-opacity-20" />
+                      <i className="fa fa-shield text-white text-opacity-20" />
                      View Commands List
                     </>
-                
                 </div>
               </Menu.Button>
             </div>
@@ -59,48 +53,46 @@ const commands = [
               leaveTo="transform opacity-0 scale-95">
               <Menu.Items className="z-1 custom-scroll absolute left-0 w-full mt-2 origin-top-right bg-black overflow-auto max-h-60 border border-black border-opacity-20 bg-opacity-95 rounded-lg p-1">
                 <div className="px-1 py-1 w-full">
-
-            {commands.map(mm => (
-
-                <Menu.Item>
-                    <button className={`w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200`}>
-                    <>
-                          <i className="fa-spin text-xl text-white text-opacity-20" />
-                         {mm}</>
-                    </button>
+                {commands.map(cmd => (
+                    <Menu.Item>
+                        <button className={`w-full rounded-lg p-2 cursor-pointer text-white flex items-center space-x-3 transition-all duration-200`}>
+                        <>
+                              <i className="fa-spin text-xl text-white text-opacity-20" />
+                             {cmd}</>
+                        </button>
                     </Menu.Item>
-            ))}
-              
-        </div>
+                ))}
+                </div>
               </Menu.Items>
             </Transition>
           </Menu>
         </div>
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-  <Link href={"https://discord.com/oauth2/authorize?client_id=862340553899507715&permissions=2147871840&scope=bot%20applications.commands"}>
-  <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite TadaBot</a>
-  </Link>
+            <Link href={"https://discord.com/api/oauth2/authorize?client_id=1077740486596571196&permissions=1374891928950&scope=bot%20applications.commands"}>
+                <a className="flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-blue-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-blue-700 to-blue-500 hover:opacity-80 transition duration-200 text-white">
+                    Invite Discord Justice
+                </a>
+            </Link>
 
-  <div className="py-10"></div>
-  
-  <Link href={"https://discord.gg/zr8hRh5Snj"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
-  </Link>
+            <div className="py-10"></div>
 
-  <div className="py-10"></div>
-  
-  <Link href={"https://top.gg/bot/862340553899507715/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote TadaBot</a>
-  </Link>
-</div>
+            <Link href={"https://discord.gg/justice"}>
+                <a className="px-6 justify-center gap-x-2 shadow-lg shadow-blue-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-blue-700 to-blue-500 hover:opacity-80 transition duration-200 text-white">
+                    Support Server
+                </a>
+            </Link>
 
+            <div className="py-10"></div>
+
+            <Link href={"https://top.gg/bot/1077740486596571196/vote"}>
+                <a className="px-6 justify-center gap-x-2 shadow-lg shadow-blue-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-blue-700 to-blue-500 hover:opacity-80 transition duration-200 text-white">
+                    Vote Discord Justice
+                </a>
+            </Link>
+        </div>
       </div>
       <div className="py-20"></div>
     </>
   )
-  }
+}
